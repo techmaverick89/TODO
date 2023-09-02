12b2ef777fb00aa10aee5ae67a40825f987f30de
root <root@Maverick>
2023-09-02 16:43:54 +0700

    2/9

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -21,7 +21,7 @@ function App() {
     return (
         <div className="App">
             <div className="container">
-                <h1>ToDo App</h1>
+                <h1>ToDo List</h1>
 
                 <div className="top">
                     <input
